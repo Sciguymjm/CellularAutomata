6a98288877335a7884a7e3dac586ff56876a0082
Sciguymjm <sciguymjm@gmail.com>
2015-09-23 21:43:27 -0400

    Cleaned up code once again

--- main.py
+++ main.py
@@ -8,23 +8,17 @@ import mingus.core.scales as scales
 from mingus.containers import instrument
 
 
-
-
-
-def format(i):
+def format_block(i):
     return "|" + "".join([u'▋' if n else u"  " for n in i]) + "|"
 
 
-def trackgen(i, length, bars, oct, instrument):
-    #what it resets to
+def trackgen(i, length, bars, octave, inst):
+    # what it resets to
     n = [False, False, False, False, False, False, False, False, False]
-    track = Track(instrument)
-    for b in range(0,(length * bars) / 4):
-        bar = Bar("C", (4,4))
-        for t in range(0,4):
-            note = NoteContainer()
-            #stop all current notes
-            fluidsynth.stop_NoteContainer(note)
+    track = Track(inst)
+    for b in range(0, (length * bars) / 4):
+        bar = Bar("C", (4, 4))
+        for t in range(0, 4):
             for ind, s in enumerate(i):
                 x = ind - 1
                 y = ind
@@ -58,35 +52,23 @@ def trackgen(i, length, bars, oct, instrument):
                     n[y] = False
             i = n
             n = [False, False, False, False, False, False, False, False, False]
-            note = NoteContainer()
-            drums = NoteContainer()
-            scale = scales.Ionian("C").ascending()
             scale = ["C", "D", "E", "G", "A"]
 
-
             for index, d in enumerate(i):
-                # if index == 9 and d:
-        #             drums.add_note(Note(26), octave=0)  #28 = snare drum, for others check https://en.wikipedia.org/wiki/General_MIDI#Percussion and subtract 10
-        #             continue
-        #         elif index >= 10 and d:
-        #             drums.add_note(Note(24), octave=0)
-        #             continue
-                if d and random.randrange(0,6) == 3:
+                if d and random.randrange(0, 6) == 3:
                     bar.place_rest(length)
                     break
                 if d:
-                    bar.place_notes(Note(list(reversed(scale))[index if index < 5 else index - 4], octave=oct if index < 5 else oct + 1),length)
+                    # strip off the top note for the melody
+                    bar.place_notes(Note(list(reversed(scale))[index if index < 5 else index - 4],
+                                         octave=octave if index < 5 else octave + 1), length)
                     break
-            print format(i)
+            print format_block(i)
         track.add_bar(bar)
     return track
 
-# initialize the soundfont
-fluidsynth.init("example.SF2")
-
-#fluidsynth.set_instrument(channel=2, bank=126, instr=0)
-#initial setup
-i = [
+# initial setup
+ini = [
     False,
     False,
     False,
@@ -97,21 +79,13 @@ i = [
     False,
     False]
 
-i[random.randrange(0,8)] = True
-
-
-
-
-
-
-
+ini[random.randrange(0, 8)] = True
 
 melody = instrument.MidiInstrument()
 melody.instrument_nr = 1
-track1 = trackgen(i, 8, 19, 4, melody)
-#midi_file_out.write_Track("track1.mid", track)
+track1 = trackgen(ini, 8, 19, 4, melody)
 
-i = [
+ini = [
     False,
     False,
     False,
@@ -122,17 +96,16 @@ i = [
     False,
     False]
 
-i[random.randrange(0,8)] = True
-
-
+ini[random.randrange(0, 8)] = True
 
 bass = instrument.MidiInstrument()
 bass.instrument_nr = 1
-track2 = trackgen(i, 4, 19, 3, bass)
+track2 = trackgen(ini, 4, 19, 3, bass)
 
 comp = Composition()
 
 comp.add_track(track1)
 comp.add_track(track2)
 
-midi_file_out.write_Composition("comp.mid",comp)
+midi_file_out.write_Composition("comp.mid", comp)
+print "Wrote to file..."
